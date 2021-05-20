6825d55b0f76f8afd51a6f8f63a932fbd17baa3d
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-05-20 11:33:42 +0530

    Update callsmusic.py

--- helpers/callsmusic.py
+++ helpers/callsmusic.py
@@ -1,8 +1,8 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-from DaisyXMusic.config import API_HASH, API_ID, SESSION_NAME
-from DaisyXMusic.services.callsmusic import queues
+from .config import API_HASH, API_ID, SESSION_NAME
+from sira import queues
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
