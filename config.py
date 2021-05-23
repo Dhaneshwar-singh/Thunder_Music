95ed0e19a87daf3064a7f41bc9b49884e591b6b9
developer-boy-sdowner <75466952+developer-boy-sdowner@users.noreply.github.com>
2021-05-23 11:52:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,3 +15,5 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
  
 ARQ_API_KEY = getenv("ARQ_API_KEY", "VKSSQH-SRNJYZ-MSOJAI-QALESR-ARQ")
+
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "DaisyXupdates")
